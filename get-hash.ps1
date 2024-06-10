3798fafe50773db2992a79ff3b341b1554e7633d
Alex Diker <23444429+alexdcodes@users.noreply.github.com>
2024-06-10 15:03:04 -0400

    Update get-hash.ps1

--- get-hash.ps1
+++ get-hash.ps1
@@ -2,12 +2,12 @@
  # paths outlined for folders for checking hashing: 
  # plans for project have this happen before a shutdown of important project files for example"
  # have it verify files integrity just before running an install against any asc file etc 
-
+ #
  function getHash
 {
-    $Path =  "C:\Users\DefaultPC\Documents\"
-    $Path =  "C:\Users\DefaultPC\Documents\"
-    $Path2 = "C:\Users\DefaultPC\Downloads\"
+    $Path =  "C:\Users\TempDevelopmentPC\Documents\"
+    $Path =  "C:\Users\TempDevelopmentPC\Documents\"
+    $Path2 = "C:\Users\TempDevelopmentPC\Downloads\"
     $Path3 = ""
     $Path4 = ""
 
@@ -21,6 +21,27 @@
     Write-Output $Path
 }
 
-Write-Output "Obtaining Folders: Documents , Downloads."
+Write-Output "SHA256SUM Obtaining Folders: Documents , Downloads."
+
+Write-Host -ForegroundColor Green "                    3 Wishes - SHA256 Powershell Checker ad -"
+
+Write-Host -ForegroundColor DarkYellow "                       _oo0oo_"
+Write-Host -ForegroundColor DarkYellow "                      o8888888o"
+Write-Host -ForegroundColor DarkYellow "                      88`" . `"88"
+Write-Host -ForegroundColor DarkYellow "                      (| -_- |)"
+Write-Host -ForegroundColor DarkYellow "                      0\  =  /0"
+Write-Host -ForegroundColor DarkYellow "                    ___/`----'\___"
+Write-Host -ForegroundColor DarkYellow "                  .' \\|     |// '."
+Write-Host -ForegroundColor DarkYellow "                 / \\|||  :  |||// \"
+Write-Host -ForegroundColor DarkYellow "                / _||||| -:- |||||- \"
+Write-Host -ForegroundColor DarkYellow "               |   | \\\  -  /// |   |"
+Write-Host -ForegroundColor DarkYellow "               | \_|  ''\---/''  |_/ |"
+Write-Host -ForegroundColor DarkYellow "               \  .-\__  '-'  ___/-. /"
+Write-Host -ForegroundColor DarkYellow "             ___'. .'  /--.--\  `. .'___"
+Write-Host -ForegroundColor DarkYellow "          .`"`" '<  `.___\_<|>_/___.' >' `"`"."
+Write-Host -ForegroundColor DarkYellow "         | | :  `- \`.;`\ _ /`;.`/ - ` : | |"
+Write-Host -ForegroundColor DarkYellow "         \  \ `_.   \_ __\ /__ _/   .-` /  /"
+Write-Host -ForegroundColor DarkYellow "     =====`-.____`.___ \_____/___.-`___.-'====="
+Write-Host -ForegroundColor DarkYellow "                       `=---='"
 
 getHash($Path)
